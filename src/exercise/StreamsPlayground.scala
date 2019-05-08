641d653abfc61d797090517beae23a80a2895ab1
Pawel Kwiatek <pawel.kwiatek@nokia.com>
2019-05-08 10:36:36 +0200

    Lazy evaluation - part 2

--- src/exercise/StreamsPlayground.scala
+++ src/exercise/StreamsPlayground.scala
@@ -8,7 +8,7 @@ abstract class MyStream[+A] {
   def tail: MyStream[A]
 
   def #::[B >: A](element: B): MyStream[B] // prepand operator
-  def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // concatenate two streams
+  def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] // concatenate two streams
 
   def foreach(f: A => Unit): Unit
   def map[B](f: A => B): MyStream[B]
@@ -35,7 +35,7 @@ object EmptyStream extends MyStream[Nothing] {
   def tail: MyStream[Nothing] = throw new NoSuchElementException
 
   def #::[B >: Nothing](element: B): MyStream[B] = new Cons(element, this)
-  def ++[B >: Nothing](anotherStream: MyStream[B]): MyStream[B] = anotherStream
+  def ++[B >: Nothing](anotherStream: => MyStream[B]): MyStream[B] = anotherStream
 
   def foreach(f: Nothing => Unit): Unit = ()
   def map[B](f: Nothing => B): MyStream[B] = this
@@ -51,7 +51,7 @@ class Cons[+A](hd: A, tl: => MyStream[A]) extends MyStream[A] {
   override lazy val tail: MyStream[A] = tl // call by need (byName + lazy)
 
   def #::[B >: A](element: B): MyStream[B] = new Cons(element, this)
-  def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
+  def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
 
   def foreach(f: A => Unit): Unit = {
     f(head)
@@ -88,4 +88,34 @@ object StreamsPlayground extends App{
   println(startFrom0.map(_ * 2).take(100).toList())
 
   println(startFrom0.flatMap(x => new Cons(x, new Cons(x + 1, EmptyStream))).take(10).toList())
+  println(startFrom0.filter(_ < 10).take(10).toList())
+
+  // Exercises on streams
+  // 1 - stream of Fibonacci numbers
+  // 2 - stream of prime numbers with Eratosthenes' sieve
+  /*
+    [2,3,4, ...]
+    filter out all numbers divisible by 2
+    [2,3,5,7,9,11,...]
+    filter out all numbers divisible by 3
+    [2,3,5,7,11,13,17]
+    filter out all numbers divisible by 5
+      ...
+   */
+
+  val fib = MyStream.from((1,1))(tuple => (tuple._2, tuple._1 + tuple._2)).map(_._1)
+  println(fib.take(10).toList())
+
+  val naturalsFrom2 = MyStream.from(2)(_ + 1)
+  val primes = MyStream.from((naturalsFrom2, 2))(tuple => (tuple._1.tail.filter(_ % tuple._1.head != 0), tuple._1.head)).map(_._2)
+  println(primes.take(20).toList())
+
+  def fibonacci(first: Int, second: Int): MyStream[Int] =
+    new Cons(first, fibonacci(second, first + second))
+  println(fibonacci(1,1).take(100).toList())
+
+  def eratosthenes(numbers: MyStream[Int]): MyStream[Int] =
+    new Cons(numbers.head, eratosthenes(numbers.tail.filter(_ % numbers.head != 0)))
+
+  println(eratosthenes(naturalsFrom2).take(100).toList())
 }
